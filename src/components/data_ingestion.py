f16f388637ee0dedfe3fab1528ca7745c8b943fa
spoorthigr <spoorthigr3@gmail.com>
2025-02-25 12:34:56 +0530

    model trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,7 @@ from dataclasses import dataclass
 from src.pipeline.exception import CustomException
 from src.pipeline.logger import logging
 from src.components.data_transformation import DataTransformation, DataTransformationConfig
+from src.components.model_trainer import ModelTrainerConfig, ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
@@ -52,4 +53,8 @@ if __name__ == "__main__":
     train_data, test_data = obj.initiate_data_ingestion()
     
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_arr, test_arr,preprocessor_path  = data_transformation.initiate_data_transformation(train_data, test_data)
+
+    ModelTrainer = ModelTrainer()
+    print(ModelTrainer.initiate_model_trainer(train_arr, test_arr))
+    
