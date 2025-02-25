69be68a01cccbfd63f255dea50133d2815606857
spoorthigr <spoorthigr3@gmail.com>
2025-02-25 20:59:17 +0530

    frontend and prediction completed

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -45,7 +45,41 @@ class ModelTrainer:
                         'AdaBoost Regressor' : AdaBoostRegressor()
                   }
                   
-                  model_report : dict = evaluate_models(x_train=x_train, y_train=y_train, x_test=x_test, y_test=y_test, models=models)
+                  params = {
+                        'Decision Tree' :{
+                              'criterion' : ['square_error', 'friedman_mse', 'absolute_error', 'poisson']
+                        },
+                        
+                        'Random Forest' : {
+                              'n_estimators' : [8,16,32,64,128,256]
+                        },
+                        
+                        'Gradient Boosting' : {
+                              'learning_rate' : [.1, .01, .05, .001],
+                              'subsample' : [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+                              'n_estimators' : [8,16,32,64,128,256]
+                        },
+                        
+                        'Linear Regression' : {},
+                        
+                        'K-Neighbor Regressor' : {
+                              'n_neighbors' : [5,7,9,11]
+                        },
+                        
+                        'XGBRegressor' : {
+                              'learning_rate' : [.1, .01, .05, .001],
+                              'n_estimators' : [8,16,32,64,128,256]
+                        },
+                        
+                        'AdaBoost Regressor' : {
+                              'learning_rate' : [.1, .01, .05, .001],
+                              'n_estimators' : [8,16,32,64,128,256]
+
+                        }
+                              
+                  }
+                  
+                  model_report : dict = evaluate_models(x_train=x_train, y_train=y_train, x_test=x_test, y_test=y_test, models=models, params=params)
                   
                   #To get the best model score from dict
                   best_model_score = max(sorted(model_report.values()))
--- src/pipeline/utils.py
+++ src/pipeline/utils.py
@@ -5,6 +5,7 @@ import pandas as pd
 from src.pipeline.exception import CustomException
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):
       try:
@@ -18,13 +19,18 @@ def save_object(file_path, obj):
       except Exception as e:
             raise CustomException(e, sys)
             
-def evaluate_models(x_train, y_train, x_test, y_test, models):
+def evaluate_models(x_train, y_train, x_test, y_test, models, params):
       try:
             report = {}
             
             for i in range(len(list(models))):
                   model = list(models.values())[i]
+                  params = params[list(models.keys())[i]]
                   
+                  gs = GridSearchCV(model, params, cv=3)
+                  gs.fit(x_train, y_train)
+                  
+                  model.set_params(**gs.best_params_)
                   model.fit(x_train, y_train)
                   
                   y_train_pred = model.predict(x_train)
@@ -37,5 +43,13 @@ def evaluate_models(x_train, y_train, x_test, y_test, models):
                   
             return report
       
+      except Exception as e:
+            raise CustomException(e, sys)
+      
+def load_object(file_path):
+      try:
+            with open(file_path, 'rb') as file_obj:
+                  return dill.load(file_obj)
+            
       except Exception as e:
             raise CustomException(e, sys)
